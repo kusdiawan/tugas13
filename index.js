a39b4fbad824a02e790a4cd36dcbf100ecebfbd6
kusdiawan <71364605+kusdiawan@users.noreply.github.com>
2020-09-21 09:18:49 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+//mendefinisikan variable array nama-nama Buah
+var namaBuah = ["Pisang", "Jeruk"];
+//menampilkan data nama-nama buah
+console.log(namaBuah);
+
+//menyisipkan data opada array
+namaBuah.splice(0,0, "Apel");
+console.log(namaBuah);
+
+//menyisipkan data opada array
+namaBuah.splice(1,0, "Mangga");
+console.log(namaBuah);
+
+
